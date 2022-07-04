6ff5d82b88cbcd8f477bb7b4d154518fe39a8030
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-07-04 20:39:41 +1200

    feat: initial nextjs page integration, _app and _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@0;1&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
